82602f1260e7acffbdc1c0a0b78aa33193ea36f0
Fiewor <johnfiewor@gmail.com>
2024-11-11 18:30:15 +0100

    make utils.ts accessible, change imports, uncomment baseUrl

--- frontend/src/components/ui/form.tsx
+++ frontend/src/components/ui/form.tsx
@@ -10,7 +10,7 @@ import {
   useFormContext,
 } from "react-hook-form";
 
-import { cn } from "../../lib/utils.ts";
+import { cn } from "@/utils.ts";
 import { Label } from "./label";
 
 const Form = FormProvider;
